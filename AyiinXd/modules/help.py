933aa765f62610df800c008bf16f860239530358
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-08-28 20:01:43 +0700

    Update help.py

--- AyiinXd/modules/help.py
+++ AyiinXd/modules/help.py
@@ -1,8 +1,15 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
+# Copyright (C) 2020 TeamDerUntergang
+""" Userbot help command for multi client """
+# SedenUserBot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
-# you may not use this file except in compliance with the License.
-#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# @Qulec tarafından yazılmıştır.
+# Thanks @Spechide
 """ Userbot help command for multi client """
 
 from time import sleep
@@ -13,8 +20,11 @@ from AyiinXd.ayiin import ayiin_cmd, eod, eor
 from Stringyins import get_string
 
 
-@ayiin_cmd(pattern="chelp(?: |$)(.*)")
-async def help(event):
+
+@ayiin_cmd(pattern="help(?: |$)(.*)")
+async def helpyins(event):
+    if event.fwd_from:
+        return
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
@@ -22,17 +32,54 @@ async def help(event):
         else:
             await eod(event, get_string("help_10").format(args, cmd))
     else:
-        user = await event.client.get_me()
-        string = ""
-        for i in CMD_HELP:
-            string += "`" + str(i)
-            string += f"`\t\t\t{ICON_HELP}\t\t\t"
-        xnxx = await eor(event, "🗿")
-        sleep(3)
-        await xnxx.edit(
-            f"**[✧ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧](https://github.com/AyiinXd/Ayiin-Userbot)**\n"
-            f"**߷ 𝙹𝚄𝙼𝙻𝙰𝙷** `{len(CMD_HELP)}` **Modules**\n"
-            f"**♕︎ 𝙾𝚆𝙽𝙴𝚁:** [{user.first_name}](tg://user?id={user.id})\n\n"
-            f"{ICON_HELP}   {string}"
-            f"\n\n☞  **𝚂𝚄𝙿𝙿𝙾𝚁𝚃** : @AyiinXdSupport\n☞  **𝙽𝙾𝚃𝙴𝚂** :  `{cmd}help yinsubot` **Untuk Melihat Modules Lainnya**"
-        )
+        AyiinUBOT = await tgbot.get_me()
+        BOT_USERNAME = AyiinUBOT.username
+        if BOT_USERNAME is not None:
+            chat = "@Botfather"
+            try:
+                results = await event.client.inline_query(  # pylint:disable=E0602
+                    BOT_USERNAME, "@nandetelethon"
+                )
+                await results[0].click(
+                    event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+                )
+                await event.delete()
+            except timout:
+                return await eor(event, f"Bot tidak menanggapi inline kueri.\nSilahkan Ketik `{cmd}restart`"
+                )
+            except noinline:
+                xx = await eor(event, "**Inline Mode Tidak aktif.**\n__Sedang Menyalakannya, Harap Tunggu Sebentar...__",
+                               )
+                async with bot.conversation(chat) as conv:
+                    try:
+                        first = await conv.send_message("/setinline")
+                        second = await conv.get_response()
+                        third = await conv.send_message(BOT_USERNAME)
+                        fourth = await conv.get_response()
+                        fifth = await conv.send_message("Search")
+                        sixth = await conv.get_response()
+                        await bot.send_read_acknowledge(conv.chat_id)
+                    except YouBlockedUserError:
+                        await event.client(UnblockRequest(chat))
+                        first = await conv.send_message("/setinline")
+                        second = await conv.get_response()
+                        third = await conv.send_message(BOT_USERNAME)
+                        fourth = await conv.get_response()
+                        fifth = await conv.send_message("Search")
+                        sixth = await conv.get_response()
+                        await bot.send_read_acknowledge(conv.chat_id)
+                    await xx.edit(
+                        f"**Berhasil Menyalakan Mode Inline**\n\n**Ketik** `{cmd}help` **lagi untuk membuka menu bantuan.**"
+                    )
+                await bot.delete_messages(
+                    conv.chat_id,
+                    [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id],
+                )
+        else:
+            await eor(
+                event,
+                "**Silahkan Buat BOT di @BotFather dan Tambahkan Var** `BOT_TOKEN` & `BOT_USERNAME`",
+            )
+Footer
+© 2022 
+     

3b093d4248cf57f8b56b11b7cd96726578e3d120
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-08-28 21:28:13 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -23,7 +23,7 @@ from AyiinXd import (
 )
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
-EOL = "EOL\nAyiin-UserBot v{}, Copyright © 2021-2022 Ayiin• <https://github.com/AyiinXd>"
+EOL = "EOL\nNande-Telethon v{}, Copyright © 2021-2022 Nande• <https://github.com/sip-Userbot>"
 MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nAyiin-UserBot v{}, Copyright © 2021-2022 Ayiin• <https://github.com/AyiinXd>"
 
 
@@ -33,13 +33,13 @@ async def ayiin_client(client):
 
 
 def multiayiin():
-    if 997461844 not in DEVS:
+    if 2062364017 not in DEVS:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
     if -1001675396283 not in GBL:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
-    if 997461844 not in DEFAULT:
+    if 2062364017 not in DEFAULT:
         LOGS.warning(EOL.format(version))
         sys.exit(1)
     failed = 0

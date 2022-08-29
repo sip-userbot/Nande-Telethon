64e97afa4e45182a94efa54073e5ac12e2788e86
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-08-29 21:51:27 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -294,7 +294,7 @@ while 0 < 6:
 
 del _BLACKLIST
 
-ch = str(b64decode("QGFsaW1iYW5nZXRfMg=="))[2:15]
+ch = str(b64decode("U3Vwb3J0TmFuZGU="))[2:15]
 gc = str(b64decode("QHN1cG9ydHNpcHVzZXJib3Q"))[2:17]
 
 while 0 < 6:

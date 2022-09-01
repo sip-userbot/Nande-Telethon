e5043068b91527e68ec64bd091bdd04b2f4e36b3
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-01 09:20:26 +0700

    Update events.py

--- AyiinXd/events.py
+++ AyiinXd/events.py
@@ -206,8 +206,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**✘ AYIIN-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/AyiinXdSupport)"
+                    text = "**✘ NANDE-TELETHON ERROR REPORT ✘**\n\n"
+                    link = "[Group Support](https://t.me/suportsipuserbot)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -216,7 +216,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @AyiinXdSupport"
+                    ftext += "\ncukup forward saja pesan ini ke @suportsipuserbot"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
